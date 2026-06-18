4e83289534c7ea14a6de067c6812362f684479c6
SOUMYA <sdasuemk@gmail.com>
2026-06-19 02:22:16 +0530

    Prompt Template

---
+++ prompttemplate/chat_prompt_template.py
@@ -0,0 +1,14 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+template = ChatPromptTemplate(
+    [
+        ("system", "You are a helpful AI bot. Your name is {name}."),
+        ("human", "Hello, how are you doing?"),
+        ("ai", "I'm doing well, thanks!"),
+        ("human", "{user_input}"),
+    ]
+)
+
+prompt = template.invoke({"name": "Robo", "user_input": "What can you do?"});
+
+print(prompt)
